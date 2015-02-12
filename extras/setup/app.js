752d1d4ba1eb631a563dfb92bbc95b37cfa8162f
Matt Keas <matthiasak@gmail.com>
2015-02-12 09:44:16 -0600

    adding Pace

--- extras/setup/app.js
+++ extras/setup/app.js
@@ -10,8 +10,9 @@ function app(){
         {url: "./dist/style.css"},
         //js
         {url: "./bower_components/jquery/dist/jquery.min.js"},
-        {url: "./bower_components/lodash/lodash.min.js"}
-        // {url: "./bower_components/backbone/backbone.js"}
+        {url: "./bower_components/lodash/lodash.min.js"},
+        {url: "./bower_components/backbone/backbone.js"},
+        {url: "./bower_components/pace/pace.min.js"}
     ).then(function(){
         document.querySelector("html").style.opacity = 1;
         // start app?
