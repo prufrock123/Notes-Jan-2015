7250d407d5b5ba5d970e5440931ddac22cc8d387
Matt Keas <matthiasak@gmail.com>
2015-03-01 13:56:10 -0600

    adding browserify, updating files to work with loader or browserify

--- extras/setup/TemplateView.js
+++ extras/setup/TemplateView.js
@@ -1,42 +1,41 @@
-;(function(){
+;(function(exports){
 
     "use strict";
 
-    var x =  (typeof Parse !== "undefined") && ("Parse") || (typeof Backbone !== "undefined") && ("Backbone");
-    if(!x) return;
+    exports.TemplateView = function createTemplateView(backboneOrParse, _, $){
+        return backboneOrParse.View.extend({
+            cache: {},
+            stream: function(url) {
+                var x = $.Deferred();
+                if (this.cache[url]) {
+                    x.resolve(this.cache[url]);
+                } else {
+                    $.get(url).then((function(d) {
+                        this.cache[url] = _.template(d);
+                        x.resolve(_.template(d));
+                    }).bind(this));
+                }
+                return x;
+            },
+            loadTemplate: function(name) {
+                return this.stream('./templates/' + name + '.html');
+            },
+            initialize: function(options) {
+                this.options = options || {};
 
-    window[x].TemplateView = window[x].View.extend({
-        cache: {},
-        stream: function(url) {
-            var x = $.Deferred();
-            if (this.cache[url]) {
-                x.resolve(this.cache[url]);
-            } else {
-                $.get(url).then((function(d) {
-                    this.cache[url] = _.template(d);
-                    x.resolve(_.template(d));
-                }).bind(this));
+                this.model && this.model.on("change", this.render.bind(this));
+                this.collection && this.collection.on("add reset remove", this.render.bind(this));
+            },
+            render: function() {
+                var self = this;
+                this.loadTemplate(this.options.view || this.view).then(function(fn) {
+                    var d = self.model || self.collection;
+                    self.el.innerHTML = fn({
+                        data: d
+                    });
+                })
             }
-            return x;
-        },
-        loadTemplate: function(name) {
-            return this.stream('./templates/' + name + '.html');
-        },
-        initialize: function(options) {
-            this.options = options || {};
+        })
+    }
 
-            this.model && this.model.on("change", this.render.bind(this));
-            this.collection && this.collection.on("add reset remove", this.render.bind(this));
-        },
-        render: function() {
-            var self = this;
-            this.loadTemplate(this.options.view || this.view).then(function(fn) {
-                var d = self.model || self.collection;
-                self.el.innerHTML = fn({
-                    data: d
-                });
-            })
-        }
-    })
-
-})();
+})(typeof module === "object" ? module.exports : window);
